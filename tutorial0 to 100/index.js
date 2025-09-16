44c73236cddf31124fcd46d6e1f0934285618bc6
Sara farahani <s.s.farahani2008@gmail.com>
2025-09-16 23:07:37 +0330

    Add files via upload

---
+++ tutorial0 to 100/index.js
@@ -0,0 +1,18 @@
+
+function  turnOnLamp()
+{
+    document.getElementById('lamp').src="/img/pic_bulboff.gif";
+       document.getElementById("temp1").style.background ="gray"
+    return
+
+ 
+}
+
+function  turnOffLamp()
+{
+    document.getElementById('lamp').src="/img/pic_bulbon.gif";
+       document.getElementById("temp1").style.background ="white"
+    return
+
+ 
+}
