1e5e617884123c33ebd0d9f1687397b8658cf759
Sara farahani <s.s.farahani2008@gmail.com>
2025-12-13 22:20:27 +0330

    Add files via upload

--- todolist.js
+++ todolist.js
@@ -3,10 +3,10 @@ const todos=[];
 const todoInput=document.querySelector(".todo-input");
 const todoForm=document.querySelector(".todo-form");
 const todolist=document.querySelector(".todo-list");
-todoInput.addEventListener("submit",addNewtodo);
+todoForm.addEventListener("submit",addNewtodo);
 function addNewtodo(e){
-    console.log(todoInput.value)
-  e.preventDefault();
+    e.preventDefault();
+
     if(!todoInput.value) return "null";
     const newTodo={
         id:Date.now(),
@@ -15,10 +15,9 @@ function addNewtodo(e){
         iscompelet:false
 
     }
-todos.push(newTodo)
+    todos.push(newTodo)
 let result="";
   
-
 todos.forEach((todo) => {
     result += `<li class="todo">
       <p class="todo__title ${todo.isCompleted && "completed"}">${
